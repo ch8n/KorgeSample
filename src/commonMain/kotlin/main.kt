3c329eb2926d343d6d2c595659f9091ef89233cd
ch8n <chetan.garg36@gmail.com>
2021-01-21 00:32:32 +0530

    controlls

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -1,7 +1,12 @@
+import com.soywiz.klock.timesPerSecond
+import com.soywiz.korev.Key
+import com.soywiz.korev.KeyEvent
 import com.soywiz.korge.Korge
 import com.soywiz.korge.view.*
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.color.RGBA
+import com.soywiz.korim.font.readBitmapFont
+import com.soywiz.korio.file.std.resourcesVfs
 import com.soywiz.korui.layout.MathEx
 
 suspend fun main() = Korge(
@@ -11,38 +16,55 @@ suspend fun main() = Korge(
         bgcolor = RGBA(253, 247, 240) //Colors["#2b2b2b"]
 ) {
 
+    val height = views.virtualHeight
+    val width = views.virtualWidth
+    val circle = circle(radius = 25.0, fill = Colors.PINK).xy(height / 2, width / 2)
+    val leftText = text(text = "LEFT", textSize = 10.0, color = Colors.RED)
+    val rightText = text(text = "LEFT", textSize = 10.0, color = Colors.BLUE)
+    val upText = text(text = "LEFT", textSize = 10.0, color = Colors.GREEN)
+    val downText = text(text = "LEFT", textSize = 10.0, color = Colors.AQUA)
 
-    val top = roundRect(views.virtualWidth, 1, 0, 0, Colors.BLACK) {
-        position(0, 10)
-        addChild(this)
+
+    addFixedUpdater(30.timesPerSecond) {
+        when {
+            views.input.keys[Key.UP] && views.input.keys[Key.LEFT] -> {
+                circle.color = Colors.BLUEVIOLET
+                circle.xy(circle.x - 1, circle.y - 1)
+            }
+
+            views.input.keys[Key.UP] && views.input.keys[Key.RIGHT] -> {
+                circle.color = Colors.GREENYELLOW
+                circle.xy(circle.x + 1, circle.y - 1)
+            }
+
+            views.input.keys[Key.DOWN] && views.input.keys[Key.RIGHT] -> {
+                circle.color = Colors.HOTPINK
+                circle.xy(circle.x + 1, circle.y + 1)
+            }
+
+            views.input.keys[Key.DOWN] && views.input.keys[Key.LEFT] -> {
+                circle.color = Colors.TAN
+                circle.xy(circle.x - 1, circle.y + 1)
+            }
+
+            views.input.keys[Key.UP] -> {
+                circle.color = upText.color
+                circle.y -= 1
+            }
+            views.input.keys[Key.DOWN] -> {
+                circle.color = downText.color
+                circle.y += 1
+            }
+            views.input.keys[Key.LEFT] -> {
+                circle.color = leftText.color
+                circle.x -= 1
+            }
+            views.input.keys[Key.RIGHT] -> {
+                circle.color = rightText.color
+                circle.x += 1
+            }
+            else -> circle.color = Colors.PINK
+        }
     }
-
-    val bottom = roundRect(views.virtualWidth, 1, 0, 0, Colors.BLACK) {
-        position(0, views.virtualHeight - 10)
-        addChild(this)
-    }
-
-    val left = roundRect(1, views.virtualHeight, 0, 0, Colors.BLACK) {
-        position(10, 0)
-        addChild(this)
-    }
-
-    val right = roundRect(1, views.virtualHeight, 0, 0, Colors.BLACK) {
-        position(views.virtualWidth - 10, 0)
-        addChild(this)
-    }
-
-
-    val roundView = roundRect(100, 100, 100, 100, Colors.PINK) {
-        position(views.virtualWidth / 2, views.virtualWidth / 2)
-        addChild(this)
-    }
-
-    roundRect(100, 100, 100, 100, Colors.GREEN) {
-        addChild(this)
-        alignRightToLeftOf(roundView,padding = 5)
-        alignBottomToBottomOf(roundView)
-    }
-
 
 }
